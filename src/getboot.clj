5a6c4f892a31c408a2a2f2792410e7d967e4178e
Josh Johnson <jjohnson@adzerk.com>
2015-04-03 15:13:31 -0400

    Initial commit

---
+++ src/getboot.clj
@@ -0,0 +1,66 @@
+(ns getboot
+  (:require 
+    [ring.adapter.jetty     :as jetty]
+    [ring.middleware.params :refer [wrap-params]]
+    [ring.util.response     :refer [file-response response redirect not-found header]]
+    [clojure.data.json :as json]
+    [tentacles.repos :as repos]
+    [clojure.java.io :as io]
+    [clojure.core.memoize :as memo])
+  (:gen-class))
+  
+(defn -get-latest-release
+  []
+  (first (repos/releases "boot-clj" "boot")))
+
+(def get-latest-release (memo/ttl -get-latest-release :ttl/threshold 86400000))
+
+(defn get-assets
+  ([release]
+    (map #(:browser_download_url %) (:assets release)))
+  ([]
+    (get-assets (get-latest-release))))
+
+(defn get-mapping
+  []
+  (let [release (get-latest-release)
+        assets (get-assets)
+        tarball-url (:tarball_url release)
+        urls (map #(str "/" (.getName (io/file %))) assets)
+        mapping (zipmap urls assets)]
+    (assoc mapping "/boot.tar.gz", tarball-url)))
+
+(defn current-version
+  "Return the current release version of boot"
+  [request]
+  (let [release (get-latest-release)
+        url (:html_url release)
+        version (:name release)
+        tag_name (:tag_name release)
+        data (json/write-str {:url url :version version :tag-name tag_name})]
+     (header (response data) "Content-type" "application/json")))
+    
+(defn redirect-to-asset
+  "Redirect the user to the assets associated with the latest release"
+  [request]
+  (let 
+    [urlmap (get-mapping)
+     target (:uri request)
+     mapped (get urlmap target)]
+     
+     (if (not (nil? mapped))
+        (redirect mapped)
+        (file-response (str "static/" target)))))
+
+(defn basic-map
+  "Basic url mapping"
+  [request]
+  (println (:uri request))
+  (case (:uri request)
+    "/" (file-response "static/index.html")
+    "/info" (current-version request)
+    (redirect-to-asset request)))
+
+(defn -main
+  [& args]
+  (jetty/run-jetty basic-map {:port 3000}))
---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $.get("/info",
+      function(data){
+        $("#version").html(data.version);
+        $("#release-link").html(data['tag-name']);
+        $("#release-link").attr("href", data.url);
+      }
+    );
+});
